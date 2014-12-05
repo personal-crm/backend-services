9d6f878a9be232cee0717e676928773fb8d68666
Loïc Knuchel <loicknuchel@gmail.com>
2014-12-05 12:08:36 +0100

    enable CORS

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,12 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def corsPreflight(all: String) = Action {
+    Ok("").withHeaders(
+      "Access-Control-Allow-Origin" -> "*",
+      "Allow" -> "*",
+      "Access-Control-Allow-Methods" -> "POST, GET, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers" -> "Origin, X-Requested-With, Content-Type, Accept, Referrer, User-Agent");
+  }
+
 }
